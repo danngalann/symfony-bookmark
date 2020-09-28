e9814f091674c7df7047f2ae5182407d6fd72088
danngalann <47415745+danngalann@users.noreply.github.com>
2020-09-28 11:14:27 +0200

    Adding and listing categories

--- src/Controller/CategoriaController.php
+++ src/Controller/CategoriaController.php
@@ -2,22 +2,64 @@
 
 namespace App\Controller;
 
+use App\Entity\Categoria;
 use App\Repository\CategoriaRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/categoria")
+ */
 class CategoriaController extends AbstractController
 {
     /**
-     * @Route("/categoria", name="categoria")
+     * @Route("/listado", name="app_list_categoria")
      */
     public function index(CategoriaRepository $categoriaRepository)
     {
         $categorias = $categoriaRepository->findAll();
         return $this->render('categoria/index.html.twig', [
-            'controller_name' => 'CategoriaController',
+            'categorias' => $categorias
         ]);
     }
 
-    
+    /**
+     * @Route("/add", name="app_add_categoria")
+     */
+    public function add(
+        CategoriaRepository $categoriaRepository,
+        EntityManagerInterface $entityManager,
+        Request $request
+    ) {
+        $categoria = new Categoria();
+        if (
+            $this->isCsrfTokenValid(
+                'categoria',
+                $request->request->get('_token')
+            )
+        ) {
+            $nombre = $request->request->get('nombre');
+            // $color = $request->request->get('color');
+
+            $categoria->setNombre($nombre);
+            if ($nombre) {
+                $entityManager->persist($categoria);
+                $entityManager->flush();                
+                $this->addFlash('success', 'Categoria creada correctamente');
+                return $this->redirectToRoute('app_list_categoria');
+            } else {
+                if(!$nombre){
+                    $this->addFlash('danger', 'Nombre es obligatorio');
+                }                
+            }
+
+            
+            // $categoria->setColor($color);
+        }
+        return $this->render('categoria/add.html.twig', [
+            'categoria' => $categoria
+        ]);
+    }
 }
