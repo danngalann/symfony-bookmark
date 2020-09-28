31d31ac2c3d2061967757f15dd02771f3c3ccd89
danngalann <47415745+danngalann@users.noreply.github.com>
2020-09-28 12:50:04 +0200

    Styling Marcador CRUD

--- src/Entity/Marcador.php
+++ src/Entity/Marcador.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\MarcadorRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=MarcadorRepository::class)
@@ -19,17 +20,20 @@ class Marcador
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
      */
     private $nombre;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
      */
     private $url;
 
     /**
      * @ORM\ManyToOne(targetEntity=Categoria::class)
      * @ORM\JoinColumn(nullable=false)
+     * @Assert\NotBlank
      */
     private $categoria;
 
